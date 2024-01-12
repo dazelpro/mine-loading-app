449ce6f57d1aa18029d03e578ce50b447da9cd9d
DAZELPRO <dazelpro@gmail.com>
2024-01-12 21:05:09 +0700

    component mine-profiel

--- apps/mine-loading-app/src/app/feature/mine-route/mine-route.component.ts
+++ apps/mine-loading-app/src/app/feature/mine-route/mine-route.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { CardModule } from 'primeng/card';
+import { MineProfielComponent } from './mine-profiel/mine-profiel.component';
+import { AllocationsComponent } from './allocations/allocations.component';
+import { TrucksComponent } from './trucks/trucks.component';
+import { LotComponent } from './lot/lot.component';
 @Component({
   selector: 'mine-workspace-mine-route',
   standalone: true,
-  imports: [TabViewModule, CardModule],
+  imports: [TabViewModule, CardModule, MineProfielComponent, AllocationsComponent, TrucksComponent, LotComponent],
   template: `
     <h1>Mine and Route Management</h1>
     
     <p-tabView>
       <p-tabPanel header="Mine Profiel">
+      <mine-workspace-mine-profiel></mine-workspace-mine-profiel>
       </p-tabPanel>
       <p-tabPanel header="Allocations">
+      <mine-workspace-allocations></mine-workspace-allocations>
       </p-tabPanel>
       <p-tabPanel header="Trucks">
+      <mine-workspace-trucks></mine-workspace-trucks>
       </p-tabPanel>
       <p-tabPanel header="LOT">
+      <mine-workspace-lot></mine-workspace-lot>
       </p-tabPanel>
     </p-tabView>
   `,
